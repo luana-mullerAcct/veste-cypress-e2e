09224678ddde5a746440598ce8db5dec004049d8
luanamuller <luana.muller@acct.global>
2024-10-31 17:17:08 -0300

    Publicacao do projeto base em meu git

---
+++ cypress/e2e/acesso.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+describe('Acesso ao site da loja', () => {
+
+  //Acessando a loja e verificando pela imagem se a mesma foi carregada
+  it('Acesso a homepage', () => {
+    cy.visit('https://www.individual.com.br')
+    //cy.get('.vtex-store-components-3-x-imageElement').should('be.visible')
+    cy.get('.cc-window').should('be.visible')
+    cy.get('.cc-btn').click()
+    //cy.get('.vtex-modal-layout-0-x-container').click()
+  })
+
+   
+ 
+})
+
